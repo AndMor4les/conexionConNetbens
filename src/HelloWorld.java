af204a8d8eeac15a12d3a506bd38e9296bcb6c3a
Andres Morales <jmoraless10@est.ups.edu.ec>
2025-10-23 20:55:06 -0500

    subiendo la informacion adicional. mi segundo commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(" implementacion de informcacion");
+        System.out.println("Para verificar los commit");
+        //*********
     }
 }
